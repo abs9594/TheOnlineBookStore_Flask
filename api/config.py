59880ccdfbddc8bffa3815fc45b240e5f672e1ef
Abhishek Singh <abs9594@gmail.com>
2021-01-14 10:39:27 +0530

    Changed procfile

--- api/config.py
+++ api/config.py
@@ -8,7 +8,7 @@ class Config:
 
 class ProductionConfig(Config):
 	DEBUG = False
-	SERVER_PORT = os.environ.get('PORT', 443)
+	SERVER_PORT = os.environ.get('PORT', 5000)
 
 class DevelopmentConfig(Config):
 	DEVELOPMENT = True
