d4685e5e62dfe896af2bebbbc30943fe843545fd
Abhishek Singh <abs9594@gmail.com>
2021-03-10 00:24:40 +0530

    added try except block

--- run.py
+++ run.py
@@ -9,8 +9,8 @@ flask_app = create_app(config_object=ProductionConfig)
 with flask_app.app_context():
 	try:
 		db.create_all()
-		if not(User.query.filter_by(user_name="abhishek").first()):
-			User.create_user(user="abhishek",email="abhishek",password="AbhishekUser007$")
+		if not(User.query.filter_by(user_name="abhishek007").first()):
+			User.create_user(user="abhishek007",email="abs9594@gmail.com",password="AbhishekUser007$")
 	except exc.IntegrityError:
 		pass
 	
