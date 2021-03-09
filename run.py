ec0317f384fa559c6246676467044b830a82ff95
Abhishek Singh <abs9594@gmail.com>
2021-03-10 00:13:01 +0530

    added try except block

--- run.py
+++ run.py
@@ -2,12 +2,18 @@ from app import create_app,db
 from app.auth.models import User
 from app.catalogue.models import Book,Publication
 from app.config import DevelopmentConfig,ProductionConfig
+from sqlalchemy import exc
 
-application = create_app(config_object=ProductionConfig)
-with application.app_context():
-	db.create_all()
-	if not(User.query.filter_by(user_name="abhishek").first()):
-		User.create_user(user="abhishek",email="abhishek",password="AbhishekUser007$")
+flask_app = create_app(config_object=ProductionConfig)
 
+with flask_app.app_context():
+	try:
+		db.create_all()
+		if not(User.query.filter_by(user_name="abhishek").first()):
+			User.create_user(user="abhishek",email="abhishek",password="AbhishekUser007$")
+	except exc.IntegrityError:
+		pass
+	
+	flask_app.run()
 # if __name__ == '__main__':
-application.run()
+	# application.run()
